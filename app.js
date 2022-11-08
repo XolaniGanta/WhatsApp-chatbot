1467b3595bbe82c1fb7d4db6f7c0caa87efb1a8e
xolanigAPS <xolanig@aps.holdings>
2022-11-08 10:17:19 +0200

    make it better again

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 process.env = require('./.env.js')(process.env.NODE_ENV || 'development');
-const PORT = process.env.PORT || 9000;
+//const PORT = process.env.PORT || 9000;
 const express = require('express');
 
 let indexRoutes = require('./index.js');
@@ -10,8 +10,8 @@ const main = async () => {
     app.use(express.urlencoded({ extended: false }));
     app.use('/', indexRoutes);
     app.use('*', (req, res) => res.status(404).send('404 Not Found'));
-    app.listen(PORT, () =>
-        console.log(`App now running and listening on port ${PORT}`)
-    );
+    app.listen(process.env.PORT,()=>{
+        console.log("Establishing connection...");
+    });
 };
 main();
