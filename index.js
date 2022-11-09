9c158ce75a132ec1dc196c4b3757d8445fcb9c13
xolanigAPS <xolanig@aps.holdings>
2022-11-09 15:57:57 +0200

    flow

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const WhatsappCloudAPI = require('whatsappcloudapi_wrapper');
 const Whatsapp = new WhatsappCloudAPI({
     accessToken: process.env.Meta_WA_accessToken,
     senderPhoneNumberId: process.env.Meta_WA_SenderPhoneNumberId,
-    WABA_ID: process.env.Meta_WA_wabaId, 
+    WABA_ID: process.env.Meta_WA_wabaId,
     graphAPIVersion: 'v15.0'
 });
 
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
     res.status(200).send("Webhook working...");
 });
 
@@ -34,18 +34,18 @@ router.get('/webhook', (req, res) => {
             return res.sendStatus(403);
         }
     } catch (error) {
-        console.error({error})
+        console.error({ error })
         return res.sendStatus(500);
     }
 });
 //meta_wa_callbackurl
 router.post('/webhook', async (req, res) => {
-   
-   // console.log('POST: Someone is pinging me!');
+
+    // console.log('POST: Someone is pinging me!');
     try {
         let data = Whatsapp.parseMessage(req.body);
 
-        console.log(JSON.stringify(data,null,2));
+        console.log(JSON.stringify(data, null, 2));
 
         if (data?.isMessage) {
             let incomingMessage = data.message;
@@ -54,26 +54,26 @@ router.post('/webhook', async (req, res) => {
             let typeOfMsg = incomingMessage.type; // extract the type of message (some are text, others are images, others are responses to buttons etc...)
             let message_id = incomingMessage.message_id; // extract the message id
 
-        //Logic
-       
-           
-        
-        if(typeOfMsg === 'text_message'){
-            let incomingTextMessage = incomingMessage.text.body;
-            let filterID = incomingTextMessage.match(/^\d+$/); //if it has numbers 
-            if(filterID === null){
-                await Whatsapp.sendText({
-                    message:`Hi ${recipientName}, Welcome to BestForU self-service. In order to continue you are required to enter your ID.`,
-                    recipientPhone: recipientPhone
-                })
+            //Logic
+
+
+
+            if (typeOfMsg === 'text_message') {
+                let incomingTextMessage = incomingMessage.text.body;
+                let filterID = incomingTextMessage.match(/^\d+$/); //if it has numbers 
+                if (filterID === null) {
+                    await Whatsapp.sendText({
+                        message: `Hi ${recipientName}, Welcome to BestForU self-service. In order to continue you are required to enter your ID.`,
+                        recipientPhone: recipientPhone
+                    })
+                }
             }
-        } 
-        if(typeOfMsg === 'text_message') {
-            let incomingTextMessage = incomingMessage.text.body;
-            let filterID = incomingTextMessage.match(/^\d+$/); //if it has numbers 
-             if(filterID != null){
-                await Whatsapp.sendSimpleButtons({
-                        message:`Choose what operation do you want to perform`,
+            if (typeOfMsg === 'text_message') {
+                let incomingTextMessage = incomingMessage.text.body;
+                let filterID = incomingTextMessage.match(/^\d+$/); //if it has numbers 
+                if (filterID != null) {
+                    await Whatsapp.sendSimpleButtons({
+                        message: `Choose what operation do you want to perform`,
                         recipientPhone: recipientPhone,
                         listOfButtons: [{
                             title: 'Pay your account',
@@ -90,15 +90,15 @@ router.post('/webhook', async (req, res) => {
 
                         ]
                     })
-                  
-              }
+
+                }
             }
 
-            if(typeOfMsg === 'simple_button_message'){
+            if (typeOfMsg === 'simple_button_message') {
                 let buttonID = incomingMessage.button_reply.id;
-                if(buttonID === 'pay_account'){
+                if (buttonID === 'pay_account') {
                     await Whatsapp.sendSimpleButtons({
-                        message:`Choose which account to settle`,
+                        message: `Choose which account to settle`,
                         recipientPhone: recipientPhone,
                         listOfButtons: [{
                             title: 'Sim Only',
@@ -118,29 +118,33 @@ router.post('/webhook', async (req, res) => {
                 }
             }
 
-            if(typeOfMsg === 'simple_button_message'){
+            if (typeOfMsg === 'simple_button_message') {
                 let buttonID = incomingMessage.button_reply.id;
 
-                if(buttonID === 'Sim_Only'){
+                if (buttonID === 'Sim_Only') {
                     await Whatsapp.sendSimpleButtons({
-                        message:`Continue to check out page to finish your payment`,
+                        message: `Continue to check out page to finish your payment`,
                         recipientPhone: recipientPhone,
-                        listOfButtons:[
+                        listOfButtons: [
                             {
                                 title: 'Checkout',
-                                id: 'check_out'  
+                                id: 'check_out'
                             }
                         ]
                     })
                 }
             }
 
+            await Whatsapp.markMessageAsRead({
+                message_id,
+            });
+
 
         }
 
         return res.sendStatus(200);
     } catch (error) {
-                console.error({error})
+        console.error({ error })
         return res.sendStatus(500);
     }
 });
