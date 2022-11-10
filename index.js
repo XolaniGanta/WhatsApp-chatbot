0f17cff2af6a1db85dd255348267563b841a152d
xolanigAPS <xolanig@aps.holdings>
2022-11-10 11:06:07 +0200

    lev

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ router.post('/webhook', async (req, res) => {
             if (typeOfMsg === 'text_message') {
                 let incomingTextMessage = incomingMessage.text.body;
                 let filterID = incomingTextMessage.match(/^\d+$/); //if it has numbers 
-                if (filterID != null && filterID.length == 13) {
+                if (filterID != null ) {
                     await Whatsapp.sendSimpleButtons({
                         message: `Choose what operation do you want to perform`,
                         recipientPhone: recipientPhone,
