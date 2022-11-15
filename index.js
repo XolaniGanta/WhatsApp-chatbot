987d79580230badd82ee162e180daaa8f9640c39
xolanigAPS <xolanig@aps.holdings>
2022-11-15 14:48:47 +0200

    ccd

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ router.post('/webhook', async (req, res) => {
             if (typeOfMsg === 'text_message') {
                 let incomingTextMessage = incomingMessage.text.body;
                 let filterID = incomingTextMessage.match(/^\d+$/);
-                if (filterID != null && filterID.length === 13) {
+                if (filterID != null) {
                     await Whatsapp.sendSimpleButtons({
                         message: `Choose what operation do you want to perform`,
                         recipientPhone: recipientPhone,
