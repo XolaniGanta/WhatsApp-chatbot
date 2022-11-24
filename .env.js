d466a6f35ef7ee054918d1a1151643d25a836d4e
xolanigAPS <xolanig@aps.holdings>
2022-11-24 14:29:55 +0200

    len

--- .env.js
+++ .env.js
@@ -13,7 +13,7 @@ const development = {
     ...process.env,
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: '9000',
-    Meta_WA_accessToken:'EAALY24IFZABUBAF6cUo1HqskEuH7AXk8dQZBYVcuAfZCfzbueliUaLx4ytbxPZAkdmNDJ7SakZB3eUoSZCjVZC36RO0DqRCU82tRLSROgGSIe2gR4hZBYV8K3LMORsptRx68gnXbGdex6JKzZCWSvW6qRGZAffVI1CMW4OhLnzVnXnUbwsVYZC7G8EfnCTtVE5EfEaQm4WoUJEJmrU2WUn1Iyty',
+    Meta_WA_accessToken:'EAALY24IFZABUBANgdPtpYNguZCpycMLcwhYWJovUvVVowXHaA3ZB9LPGaJ38wGsTyCGHhsXaGciSF79pJZAQMGsgEHpzE2BJhyHfpnWc6ryTMTJJ5Ai60ZCrvQHf43njq8zBDdMvRZCf6rH0bfXFiWwZCIrPZAyBLV2lZBtFVqcir4vay1Vvz1Jqb4RQWtzXI4t7ovkzOWCYsIvZBCg40tIWrj',
     Meta_WA_SenderPhoneNumberId: '100501282880988',
     Meta_WA_wabaId: '110726525175448',
     Meta_WA_VerifyToken: 'APS',
